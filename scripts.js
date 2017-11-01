671658f42705c51d869f1a2819bd3b17658833b9
piekario13 <piekario13@gmail.com>
2017-11-01 17:34:37 +0100

    tablice

---
+++ scripts.js
@@ -0,0 +1,9 @@
+var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
+var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
+var allNames = maleNames.concat(femaleNames);
+console.log(allNames);
+var newName = 'Marian';
+if (allNames.indexOf(newName) === -1);
+console.log(allNames.indexOf(newName));
+  var newName = allNames.push('Marian');
+console.log(allNames);
